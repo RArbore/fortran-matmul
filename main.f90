95fbb174f89a45a218b88743424a09b977ec8ab0
Russel Arbore <russel.jma@gmail.com>
2021-11-26 12:51:33 -0600

    Naive matmul

--- main.f90
+++ main.f90
@@ -2,23 +2,31 @@ program matmul
   implicit none
 
   integer, dimension(8) :: seed
-  integer :: i
-  integer :: j
-  real, dimension(1000, 1000) :: a
-  real, dimension(1000, 1000) :: b
-  real, dimension(1000, 1000) :: c
+  integer :: i, j, k
+  integer :: before, after, rate, max
+  real, dimension(1000, 1000) :: a, b, c
 
+  call system_clock(count_max = max, count_rate = rate)
   seed = 760013
   call random_seed(put = seed)
   do i = 1, 1000
-    do j = 1, 1000
-      call random_number(a(i, j))
-      call random_number(b(i, j))
-      print *, a(i, j), b(i, j)
-    end do
+     do j = 1, 1000
+        call random_number(a(i, j))
+        call random_number(b(i, j))
+     end do
   end do
-  c(:, :) = 0
   
+  call system_clock(before)
+  do i = 1, 1000
+     do j = 1, 1000
+        c(i, j) = 0
+        do k = 1, 1000
+           c(i, j) = c(i, j) + a(i, k) * b(k, j)
+        end do
+     end do
+  end do
+  call system_clock(after)
 
+  print *, "Time: ", real(after - before) / real(rate)
   
 end program matmul
