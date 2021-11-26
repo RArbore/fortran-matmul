574a4dd4831712cc2a81bbddc6dc44449fd52954
Russel Arbore <russel.jma@gmail.com>
2021-11-26 16:14:05 -0600

    Recursive def + optims

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CC=gfortran -Ofast -cpp
+CC=gfortran -g -cpp -O3 -march=native -ftree-vectorize -funroll-loops -ffast-math
 
 matmul: main.f90
 	$(CC) $^ -o $@
--- main.f90
+++ main.f90
@@ -1,4 +1,5 @@
-#define SIZE 1024
+#define SIZE 2048
+#define CUTOFF 1024
 
 program matmul
   implicit none
@@ -19,28 +20,47 @@ program matmul
   end do
   
   call system_clock(before)
-  c = matmul_func(a, b)
+  c = matmul_func(a, b, SIZE, SIZE / 2)
   call system_clock(after)
 
   print *, "Time: ", real(after - before) / real(rate)
   
 contains
 
-function matmul_func(a, b) result(c)
+recursive function matmul_func(a, b, n, h) result(c)
   implicit none
 
-  integer :: i, j, k
-  real, dimension(SIZE, SIZE), intent(in) :: a, b
-  real, dimension(SIZE, SIZE) :: c
+  integer :: i, j, k, hp, hh
+  integer, intent(in) :: n, h
+  real, dimension(n, n), intent(in) :: a, b
+  real, dimension(h, h) :: a11, a12, a21, a22, b11, b12, b21, b22
+  real, dimension(n, n) :: c
+  hp = h + 1
+  hh = h / 2
 
-  c = 0
-  do j = 1, SIZE
-     do k = 1, SIZE
-        do i = 1, SIZE
-           c(i, j) = c(i, j) + a(i, k) * b(k, j)
+  if (n < CUTOFF) then
+     c = 0
+     do j = 1, n
+        do k = 1, n
+           do i = 1, n
+              c(i, j) = c(i, j) + a(i, k) * b(k, j)
+           end do
         end do
      end do
-  end do
+  else
+     a11 = a(1:h, 1:h)
+     a12 = a(1:h, hp:n)
+     a21 = a(hp:n, 1:h)
+     a22 = a(hp:n, hp:n)
+     b11 = b(1:h, 1:h)
+     b12 = b(1:h, hp:n)
+     b21 = b(hp:n, 1:h)
+     b22 = b(hp:n, hp:n)
+     c(1:h, 1:h) = matmul_func(a11, b11, h, hh) + matmul_func(a12, b21, h, hh)
+     c(1:h, hp:n) = matmul_func(a11, b12, h, hh) + matmul_func(a12, b22, h, hh)
+     c(hp:n, 1:h) = matmul_func(a21, b11, h, hh) + matmul_func(a22, b21, h, hh)
+     c(hp:n, hp:n) = matmul_func(a22, b12, h, hh) + matmul_func(a22, b22, h, hh)
+  end if
 
 end function matmul_func
 
