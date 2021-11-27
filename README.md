32c8725049a288d71c939cb2143793a96f77bc01
Russel Arbore <russel.jma@gmail.com>
2021-11-27 09:21:27 -0600

    README + license

---
+++ README.md
@@ -0,0 +1,4 @@
+## Fortran Matrix Multiply
+This is a fast(-ish) matrix multiply implemented in fortran.
+
+fortran-matmul is free / libre software licensed under the GPL v3.0.
--- main.f90
+++ main.f90
@@ -1,3 +1,15 @@
+!   This file is part of fortran-matmul.
+!   fortran-matmul is free software: you can redistribute it and/or modify
+!   it under the terms of the GNU General Public License as published by
+!   the Free Software Foundation, either version 3 of the License, or
+!   any later version.
+!   fortran-matmul is distributed in the hope that it will be useful,
+!   but WITHOUT ANY WARRANTY; without even the implied warranty of
+!   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!   GNU General Public License for more details.
+!   You should have received a copy of the GNU General Public License
+!   along with fortran-matmul. If not, see <https://www.gnu.org/licenses/>.
+
 #define SIZE 8192
 #define CUTOFF 1024
 
