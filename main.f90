21c011a3af68fbcce16966c316f252009a1883c3
Russel Arbore <russel.jma@gmail.com>
2021-11-28 17:50:18 -0600

    Proper OpenMP parallelism (on-par runtime w/ numpy on 8kx8k)

--- main.f90
+++ main.f90
@@ -11,9 +11,10 @@
 !   along with fortran-matmul. If not, see <https://www.gnu.org/licenses/>.
 
 #define SIZE 8192
-#define CUTOFF 1024
+#define CUTOFF 512
 
 program matmul
+  use omp_lib
   implicit none
 
   integer, dimension(8) :: seed
@@ -53,33 +54,68 @@ recursive function matmul_func(a, b, n, h, d) result(c)
 
   if (n < CUTOFF) then
      c = 0
-     do concurrent (j = 1:n)
+     !$OMP PARALLEL
+     !$OMP DO
+     do j = 1, n
         do k = 1, n
            do i = 1, n
               c(i, j) = c(i, j) + a(i, k) * b(k, j)
            end do
         end do
      end do
+     !$OMP END DO
+     !$OMP END PARALLEL
   else
+     !$OMP PARALLEL
+     !$OMP SECTIONS
+     !$OMP SECTION
      a11 = a(1:h, 1:h)
+     !$OMP SECTION
      a12 = a(1:h, hp:n)
+     !$OMP SECTION
      a21 = a(hp:n, 1:h)
+     !$OMP SECTION
      a22 = a(hp:n, hp:n)
+     !$OMP SECTION
      b11 = b(1:h, 1:h)
+     !$OMP SECTION
      b12 = b(1:h, hp:n)
+     !$OMP SECTION
      b21 = b(hp:n, 1:h)
+     !$OMP SECTION
      b22 = b(hp:n, hp:n)
+     !$OMP END SECTIONS
+     !$OMP END PARALLEL
+     !$OMP PARALLEL
+     !$OMP SECTIONS
+     !$OMP SECTION
      m1 = matmul_func(a11 + a22, b11 + b22, h, hh, dp)
+     !$OMP SECTION
      m2 = matmul_func(a21 + a22, b11, h, hh, dp)
+     !$OMP SECTION
      m3 = matmul_func(a11, b12 - b22, h, hh, dp)
+     !$OMP SECTION
      m4 = matmul_func(a22, b21 - b11, h, hh, dp)
+     !$OMP SECTION
      m5 = matmul_func(a11 + a12, b22, h, hh, dp)
+     !$OMP SECTION
      m6 = matmul_func(a21 - a11, b11 + b12, h, hh, dp)
+     !$OMP SECTION
      m7 = matmul_func(a12 - a22, b21 + b22, h, hh, dp)
+     !$OMP END SECTIONS
+     !$OMP END PARALLEL
+     !$OMP PARALLEL
+     !$OMP SECTIONS
+     !$OMP SECTION
      c(1:h, 1:h) = m1 + m4 - m5 + m7
+     !$OMP SECTION
      c(1:h, hp:n) = m3 + m5
+     !$OMP SECTION
      c(hp:n, 1:h) = m2 + m4
+     !$OMP SECTION
      c(hp:n, hp:n) = m1 - m2 + m3 + m6
+     !$OMP END SECTIONS
+     !$OMP END PARALLEL
   end if
 
 end function matmul_func
